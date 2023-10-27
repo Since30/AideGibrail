750cbce687fc3e6731ada807ae7c6bae43033ffe
Saflou <dz-siross@hotmail.fr>
2023-10-27 16:00:19 +0200

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify/edge-functions";
+
+export default defineConfig({
+  output: "server",
+  adapter: netlify(),
+});
